07c8c3b4a7f7b824cfe2364d81f43124a8257f88
DRIPS MEMES BLENNIE BLESSED TUNA <reinhardtuna@gmail.com>
2023-03-14 12:42:34 +0200

    Oooh Dad it's me Drips

---
+++ tuna/color.js
@@ -0,0 +1,36 @@
+// ⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈
+//▮ZIM BOT INC 2023 ®️ALL RIGHTS RESERVED
+//▮
+//▮FORK AND DON'T FORGET TO GIVE A STAR
+//▮
+//▮THIS SOFTWARE IS UNDER ZIMBOT
+//▮
+//▮REPORT ABUSE OF THIS SOFTWARE EMAIL US
+//▮support@zimbot.me
+//▮WHATSAPP US : +263 776 558 634
+//▮YOUTUBE CHANNELL: https://youtube.com/@zim-bot
+//▮
+//╰▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀
+//
+//┏━━━━━━━━━━━━━━━━━━━━━━━━━
+//┃THIS SOFTWARE INCLUDES 
+//┃SOME ENCRYPTED FILES
+//┃
+//┃THANKS FOR CHOOSING ZIMBOT
+//┃I WROTE THIS SCRIPT BY MYSELF THIS SCRIPT IS FOR EVERYONE DONT SELL IT
+//┗━━━━━━━━━━━━━━━━━━━━━━━━━
+//
+const chalk = require('chalk')
+
+const color = (text, color) => {
+    return !color ? chalk.green(text) : chalk.keyword(color)(text)
+}
+
+const bgcolor = (text, bgcolor) => {
+	return !bgcolor ? chalk.green(text) : chalk.bgKeyword(bgcolor)(text)
+}
+
+module.exports = {
+	color,
+	bgcolor
+}
